d54189349ad2010023a9745688a680919b4e7d98
shahsawar <sskhan999999999999@gmail.com>
2026-09-26 01:55:50 +0500

    fix: correct typo in CORS origin URL for Vercel app

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
     origin: [
         "http://localhost:3000",
         "http://192.168.1.18:3000",
-        "https://aethermangamant.vercel.app"
+        "https://aethermanagment.vercel.app/"
     ],
     credentials: true
 }))
